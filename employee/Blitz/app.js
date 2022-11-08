9669a6e77b7e78672e00cc57ca9f22549c479acc
nime1999 <88612197+nime1999@users.noreply.github.com>
2022-11-08 11:34:15 +0530

    Add files via upload

---
+++ employee/Blitz/app.js
@@ -0,0 +1,5 @@
+function validate() {
+    let employeeid = document.getElementById('employeeid').value;
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+}
