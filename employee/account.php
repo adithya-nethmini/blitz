ebac3e1300d748812923bc52db081a43b8b941c9
Adithya Nethmini <91539930+adithya-nethmini@users.noreply.github.com>
2022-11-11 12:25:03 +0530

    Update account.php
    updated with nav side bar

--- employee/account.php
+++ employee/account.php
@@ -23,6 +23,35 @@
     <title>My Account</title>
 </head>
 <body>
+	<nav>
+        <input type="checkbox" id="check">
+        <label for="check" class="check-bar">
+            <i class="fa fa-bars" aria-hidden="true"></i>
+        </label>
+        <label><a class="a-logo"href="../home/home.php"><img src="../images/Blitz - Logo - white.png" class="logo"></a>
+        </label>
+        <ul>
+            <li><a class="active" href="home.php"></i>Home</a></li>    
+            <li><a href="About.php">About</a></li>    
+            <li><a href="help.php">Help</a></li>    
+            <li><a href=""><i class="fa fa-bell" aria-hidden="true"></i></a></li>    
+            <li><a href="../employee/account.php"><i style="font-size:26px" class="fa fa-user" aria-hidden="true"></i></a></li>    
+        </ul>
+    </nav>
+    <input type="checkbox" id="side-check">
+    <label for="side-label">
+        <i class="fa fa-bars" aria-hidden="true" id="side-bar"></i>
+        <i class="fa fa-times" aria-hidden="true" id="side-cancel"></i>
+    </label>
+    <div class="sidebar">
+        <ul>
+            <li><a href="../task/task-manager.php"><i class="fa fa-pencil-square-o" aria-hidden="true"></i>Edit Profile</a></li>
+            <li><a href="../task/task-manager.php"><i class="fa fa-check-square" aria-hidden="true"></i> Offer Availability</a></li>
+            <li><a href="../task/task-manager.php"><i class="fa fa-qrcode" aria-hidden="true"></i>QR Code</a></li>
+            <li><a href="../task/task-manager.php"><i class="fa fa-tasks" aria-hidden="true"></i>Manage Task</a></li>
+            <li><a href="../task/task-manager.php"><i class="fa fa-line-chart" aria-hidden="true"></i>Performance Result</a></li>
+        </ul>
+    </div>
     <div class="page">
 
         <nav>
